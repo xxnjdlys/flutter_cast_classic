0bde8105856d6d32ecce73bcfa11eeb3957a5ece
xxnjdlys <xxnjdlys@gmail.com>
2018-08-24 15:37:28 +0800

    bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class CastClassicApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: DashboardPage(),
     );
   }
--- lib/ui/page/dashboard/dashboard.dart
+++ lib/ui/page/dashboard/dashboard.dart
@@ -12,19 +12,123 @@ class DashboardPage extends StatefulWidget {
   }
 }
 
-class DashboardPageState extends State<DashboardPage> {
+class NavigationIconView {
+  NavigationIconView({
+    Widget icon,
+    Widget activeIcon,
+    String title,
+    Color color,
+    TickerProvider vsync,
+  })  : _icon = icon,
+        _color = color,
+        _title = title,
+        item = new BottomNavigationBarItem(
+          icon: icon,
+          activeIcon: activeIcon,
+          title: new Text(title),
+          backgroundColor: color,
+        ),
+        controller = new AnimationController(
+          duration: kThemeAnimationDuration,
+          vsync: vsync,
+        ) {
+    _animation = new CurvedAnimation(
+      parent: controller,
+      curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+  }
+
+  final Widget _icon;
+  final Color _color;
+  final String _title;
+  final BottomNavigationBarItem item;
+  final AnimationController controller;
+  CurvedAnimation _animation;
+
+  FadeTransition transition(
+      BottomNavigationBarType type, BuildContext context) {
+    Color iconColor;
+
+    if (type == BottomNavigationBarType.fixed) {
+      iconColor = _color;
+    } else {
+      final ThemeData themeData = Theme.of(context);
+      iconColor = themeData.brightness == Brightness.light
+          ? themeData.primaryColor
+          : themeData.accentColor;
+    }
+
+    return new FadeTransition(
+      opacity: _animation,
+      child: new SlideTransition(
+        position: new Tween<Offset>(
+          begin: const Offset(0.0, 0.02), // Slightly down.
+          end: Offset.zero,
+        ).animate(_animation),
+        child: new IconTheme(
+          data: new IconThemeData(
+            color: iconColor,
+            size: 120.0,
+          ),
+          child: new Semantics(
+            label: 'Placeholder for $_title tab',
+            child: _icon,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final IconThemeData iconTheme = IconTheme.of(context);
+    return new Container(
+      margin: const EdgeInsets.all(4.0),
+      width: iconTheme.size - 8.0,
+      height: iconTheme.size - 8.0,
+      color: iconTheme.color,
+    );
+  }
+}
+
+class CustomInactiveIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final IconThemeData iconTheme = IconTheme.of(context);
+    return new Container(
+        margin: const EdgeInsets.all(4.0),
+        width: iconTheme.size - 8.0,
+        height: iconTheme.size - 8.0,
+        decoration: new BoxDecoration(
+          border: new Border.all(color: iconTheme.color, width: 2.0),
+        ));
+  }
+}
+
+class DashboardPageState extends State<DashboardPage>
+    with TickerProviderStateMixin {
   Size deviceSize;
+  int _currentIndex = 0;
+
+  BottomNavigationBarType _type = BottomNavigationBarType.fixed;
+  List<NavigationIconView> _navigationViews;
+
   Widget appBarColumn(BuildContext context) => SafeArea(
         child: Padding(
 //          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 10.0),
-          padding: const EdgeInsets.only(left: 4.0, right: 4.0, top: 16.0),
+          padding: const EdgeInsets.only(left: 4.0, right: 4.0, top: 4.0),
           child: new Column(
             children: <Widget>[
               new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   new Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.only(
+                      left: 8.0,
+                      right: 8.0,
+                    ),
                     child: new ProfileTile(
                       title: "Cast Classic",
                       subtitle: "",
@@ -39,7 +143,7 @@ class DashboardPageState extends State<DashboardPage> {
                     onPressed: () {
                       print("hi");
                     },
-                  )
+                  ),
                 ],
               ),
             ],
@@ -134,7 +238,62 @@ class DashboardPageState extends State<DashboardPage> {
         ),
       );
 
-  Widget balanceCard() => Padding(
+  Widget buildMainFragment() => Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          LoginBackground(
+            showIcon: false,
+          ),
+          allCards(context),
+        ],
+      );
+
+  Widget buildMoreFragment() => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.orange,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          actions: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+              child: new IconButton(
+                icon: new Icon(
+                  Icons.tv,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  print("hi");
+                },
+              ),
+            ),
+          ],
+          centerTitle: true,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text(
+            "My",
+            style: TextStyle(
+              fontFamily: UIData.ralewayFont,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: new Center(
+          child: new Text("Comming Soon ..."),
+        ),
+      ));
+
+  Widget normalCard() => Padding(
         padding: const EdgeInsets.all(8.0),
         child: Card(
           elevation: 2.0,
@@ -148,8 +307,12 @@ class DashboardPageState extends State<DashboardPage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      "This is title",
-                      style: TextStyle(fontFamily: UIData.ralewayFont),
+                      "Ad Title",
+                      style: TextStyle(
+                        fontFamily: UIData.ralewayFont,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
                     ),
                     Material(
                       color: Colors.black,
@@ -162,7 +325,7 @@ class DashboardPageState extends State<DashboardPage> {
                           right: 24.0,
                         ),
                         child: Text(
-                          "Btn Text",
+                          "View Details",
                           style: TextStyle(
                               color: Colors.white,
                               fontFamily: UIData.ralewayFont),
@@ -176,8 +339,8 @@ class DashboardPageState extends State<DashboardPage> {
                   style: TextStyle(
                       fontFamily: UIData.ralewayFont,
                       fontWeight: FontWeight.w700,
-                      color: Colors.green,
-                      fontSize: 25.0),
+                      color: Colors.black54,
+                      fontSize: 14.0),
                 ),
               ],
             ),
@@ -200,29 +363,98 @@ class DashboardPageState extends State<DashboardPage> {
             SizedBox(
               height: deviceSize.height * 0.01,
             ),
-            balanceCard(),
-            balanceCard(),
-            balanceCard(),
-            balanceCard(),
-            balanceCard(),
+            normalCard(),
+            normalCard(),
+            normalCard(),
+            normalCard(),
+            normalCard(),
+            normalCard(),
           ],
         ),
       );
 
   @override
+  void initState() {
+    super.initState();
+    _navigationViews = <NavigationIconView>[
+      new NavigationIconView(
+        icon: const Icon(Icons.access_alarm),
+        title: 'Browser',
+//        color: Colors.deepPurple,
+        vsync: this,
+      ),
+      new NavigationIconView(
+        activeIcon: const Icon(Icons.cloud),
+        icon: const Icon(Icons.cloud_queue),
+        title: 'More',
+//        color: Colors.teal,
+        vsync: this,
+      ),
+//      new NavigationIconView(
+//        activeIcon: new CustomIcon(),
+//        icon: new CustomInactiveIcon(),
+//        title: 'Box',
+//        color: Colors.deepOrange,
+//        vsync: this,
+//      ),
+    ];
+
+    for (NavigationIconView view in _navigationViews)
+      view.controller.addListener(_rebuild);
+
+    _navigationViews[_currentIndex].controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    for (NavigationIconView view in _navigationViews) view.controller.dispose();
+    super.dispose();
+  }
+
+  void _rebuild() {
+    setState(() {
+      // Rebuild in order to animate views.
+    });
+  }
+
+  static const int BROWSER = 0;
+  static const int MORE = 1;
+
+  Widget _getCurrentFragment(int position) {
+    Widget widget;
+    switch (position) {
+      case BROWSER:
+        widget = buildMainFragment();
+        break;
+      case MORE:
+        widget = buildMoreFragment();
+        break;
+    }
+    return widget;
+  }
+
+  @override
   Widget build(BuildContext context) {
     deviceSize = MediaQuery.of(context).size;
+
+    final BottomNavigationBar botNavBar = new BottomNavigationBar(
+      items: _navigationViews
+          .map((NavigationIconView navigationView) => navigationView.item)
+          .toList(),
+      currentIndex: _currentIndex,
+      type: _type,
+      onTap: (int index) {
+        setState(() {
+          _navigationViews[_currentIndex].controller.reverse();
+          _currentIndex = index;
+          _navigationViews[_currentIndex].controller.forward();
+        });
+      },
+    );
+
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          LoginBackground(
-//            image: Icons.search,
-            showIcon: false,
-          ),
-          allCards(context),
-        ],
-      ),
+      body: _getCurrentFragment(_currentIndex),
+      bottomNavigationBar: botNavBar,
     );
   }
 }
